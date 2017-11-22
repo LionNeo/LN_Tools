49ec2c66d4a38ab4cf59c705e390ab7ba4387ffa
Lion•Neo <123624331@qq.com>
2017-11-22 14:53:50 +0800

    update LNLog

---
+++ LN_Tools/LNLog/NSDictionary+LNLog.h
@@ -0,0 +1,13 @@
+//
+//  NSDictionary+LNLog.h
+//  test_test
+//
+//  Created by Lion•Neo on 2017/11/22.
+//  Copyright © 2017年 Lion•Neo. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSDictionary (LNLog)
+
+@end
